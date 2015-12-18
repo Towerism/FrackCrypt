15906ff2f685b0617ed39ed2b0335bbbb7e666a1
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-18 15:55:33 -0600

    Add cmake modules

---
+++ cmake/gtest.cmake
@@ -0,0 +1,43 @@
+########################### GTEST
+# Enable ExternalProject CMake module
+INCLUDE(ExternalProject)
+
+# Set default ExternalProject root directory
+SET_DIRECTORY_PROPERTIES(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/third_party)
+
+# Add gtest
+# http://stackoverflow.com/questions/9689183/cmake-googletest
+ExternalProject_Add(
+    googletest
+    URL http://googletest.googlecode.com/files/gtest-1.6.0.zip
+    # TIMEOUT 10
+    # # Force separate output paths for debug and release builds to allow easy
+    # # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
+    # CMAKE_ARGS -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=DebugLibs
+    #            -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=ReleaseLibs
+    #            -Dgtest_force_shared_crt=ON
+    # Disable install step
+    INSTALL_COMMAND ""
+    # Wrap download, configure and build steps in a script to log output
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON)
+
+# Specify include dir
+ExternalProject_Get_Property(googletest source_dir)
+set(GTEST_INCLUDE_DIR ${source_dir}/include)
+
+# Library
+ExternalProject_Get_Property(googletest binary_dir)
+set(GTEST_LIBRARY_PATH ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest.a)
+set(GTEST_LIBRARY gtest)
+set(GTEST_MAIN_PATH ${binary_dir}/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main.a)
+set(GTEST_MAIN gtest_main)
+add_library(${GTEST_LIBRARY} UNKNOWN IMPORTED)
+add_library(${GTEST_MAIN} UNKNOWN IMPORTED)
+set_property(TARGET ${GTEST_LIBRARY} PROPERTY IMPORTED_LOCATION
+                ${GTEST_LIBRARY_PATH} )
+set_property(TARGET ${GTEST_MAIN} PROPERTY IMPORTED_LOCATION
+                ${GTEST_MAIN_PATH} )
+add_dependencies(${GTEST_LIBRARY} googletest)
+add_dependencies(${GTEST_MAIN} googletest)
