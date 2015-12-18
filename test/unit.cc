50b3a5f93635ad690926d8d0526e5e51d0a1a64e
Martin Fracker <martin.frackerjr@gmail.com>
2015-12-18 13:37:22 -0600

    Added stub tests

---
+++ test/unit.cc
@@ -0,0 +1,6 @@
+#include "google/gtest/include/gtest/gtest.h"
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/unit/test_stub.cc
@@ -0,0 +1,9 @@
+#include "google/gtest/include/gtest/gtest.h"
+
+int sum(int a, int b) {
+  return a + b;
+}
+
+TEST(Stub, Sum) {
+  EXPECT_EQ(5, sum(2, 3));
+}
