0c90dc28ee1bfa62a77a8cdafef5dc4e9f81220c
Martin Fracker <martin.frackerjr@gmail.com>
2016-01-11 12:57:45 -0600

    Add copyright claim

--- frackcrypt/tools/base64/base64.cc
+++ frackcrypt/tools/base64/base64.cc
@@ -1,3 +1,11 @@
+//  Copyright 2016 Martin Fracker, Jr.
+//  All Rights Reserved.
+// 
+//  FrackCrypt is free software, released under the terms
+//  of the GNU General Public License v3. Please see the
+//  file LICENSE in the root directory or visit
+//  www.gnu.org/licenses/gpl-3.0.en.html for license terms.
+
 #include "base64.hh"
 
 namespace FrackCrypt {
